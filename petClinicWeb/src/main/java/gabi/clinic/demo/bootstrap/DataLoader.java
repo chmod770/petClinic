2f5db5a1047feb30797ba1eda8224a640c2ec310
gabi <gabon95@o2.pl>
2018-11-24 19:07:56 +0100

    Added bootstrap data this closes #9

--- petClinicWeb/src/main/java/gabi/clinic/demo/bootstrap/DataLoader.java
+++ petClinicWeb/src/main/java/gabi/clinic/demo/bootstrap/DataLoader.java
@@ -1,5 +1,7 @@
 package gabi.clinic.demo.bootstrap;
 
+import gabi.clinic.demo.model.Owner;
+import gabi.clinic.demo.model.Vet;
 import gabi.clinic.demo.services.OwnerService;
 import gabi.clinic.demo.services.VetService;
 import gabi.clinic.demo.services.map.OwnerServiceMap;
@@ -21,6 +23,37 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Tony");
+        owner1.setLastName("Hawk");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner1.setId(2L);
+        owner1.setFirstName("Bob");
+        owner1.setLastName("Burnquist");
+        ownerService.save(owner2);
+
+        System.out.println("Loaded owners...");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Anthony");
+        vet1.setLastName("Robbins");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet1.setId(2L);
+        vet1.setFirstName("Doctor");
+        vet1.setLastName("Queen");
+
+        vetService.save(vet2);
+
+        System.out.println("Loaded vets...");
+
 
     }
 }
