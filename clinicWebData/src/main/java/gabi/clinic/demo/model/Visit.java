5439931f819eda08018023a215806cd1489f707a
gabi <gabon95@o2.pl>
2018-11-28 19:44:56 +0100

    Added Pet, PetType and Visit entities. Closes #22

--- clinicWebData/src/main/java/gabi/clinic/demo/model/Visit.java
+++ clinicWebData/src/main/java/gabi/clinic/demo/model/Visit.java
@@ -6,6 +6,9 @@ import lombok.Setter;
 import java.time.LocalDate;
 
 @Getter @Setter
-public class PetType extends BaseEntity{
-    private String name;
+public class Visit extends BaseEntity{
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
 }
