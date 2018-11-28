44f2fe3a1671f805434b8634558cff44c0363166
gabi <gabon95@o2.pl>
2018-11-28 19:53:38 +0100

    Added properties. Closes #24

--- clinicWebData/src/main/java/gabi/clinic/demo/model/Owner.java
+++ clinicWebData/src/main/java/gabi/clinic/demo/model/Owner.java
@@ -7,5 +7,10 @@ import java.util.Set;
 
 @Getter @Setter
 public class Owner extends Person {
+
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
 }
