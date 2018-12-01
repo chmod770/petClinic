e01e4091f297b07f16426d97df298e412e62b582
gabi <gabon95@o2.pl>
2018-12-01 15:53:31 +0100

    Converted Vets to JPA Entity. Closes #33

--- clinicWebData/src/main/java/gabi/clinic/demo/model/Speciality.java
+++ clinicWebData/src/main/java/gabi/clinic/demo/model/Speciality.java
@@ -4,7 +4,16 @@ package gabi.clinic.demo.model;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
+
+    @Column(name = "description")
     private String description;
 }
