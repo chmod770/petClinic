eda04e62dd228318b74cf7891fee6c01f9a4485c
gabi <gabon95@o2.pl>
2018-12-01 17:35:26 +0100

    Created JPA repository for PetTypes. Closes #38

---
+++ clinicWebData/src/main/java/gabi/clinic/demo/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,49 @@
+package gabi.clinic.demo.services.springdatajpa;
+
+import gabi.clinic.demo.model.Pet;
+import gabi.clinic.demo.model.PetType;
+import gabi.clinic.demo.repositories.PetTypeRepository;
+import gabi.clinic.demo.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService{
+
+    PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        HashSet<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
