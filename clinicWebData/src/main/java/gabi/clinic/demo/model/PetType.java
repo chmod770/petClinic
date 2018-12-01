35ee464b1996574f6d99a8aa4761a8dcab32550e
gabi <gabon95@o2.pl>
2018-12-01 17:16:56 +0100

    Added owner jpa repository. Closes #36

--- clinicWebData/src/main/java/gabi/clinic/demo/model/PetType.java
+++ clinicWebData/src/main/java/gabi/clinic/demo/model/PetType.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
 
--- clinicWebData/src/main/java/gabi/clinic/demo/repositories/OwnerRepository.java
+++ clinicWebData/src/main/java/gabi/clinic/demo/repositories/OwnerRepository.java
@@ -3,5 +3,7 @@ package gabi.clinic.demo.repositories;
 import gabi.clinic.demo.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+     Owner findByLastName(String lastName);
 }
