a81c89e1ba0031bd529900dc6bbd0f43b66265c2
gabi <gabon95@o2.pl>
2018-12-02 13:42:02 +0100

    Added Spring Data repository for Speciality. Closes #40

---
+++ clinicWebData/src/main/java/gabi/clinic/demo/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,48 @@
+package gabi.clinic.demo.services.springdatajpa;
+
+import gabi.clinic.demo.model.Speciality;
+import gabi.clinic.demo.repositories.SpecialityRepository;
+import gabi.clinic.demo.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springbootdata")
+public class SpecialitySDJpaService implements SpecialityService{
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        HashSet<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
