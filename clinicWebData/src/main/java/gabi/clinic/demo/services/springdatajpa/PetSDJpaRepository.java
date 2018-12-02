80c533e849de68354981a6b5ed99b7046ed518af
gabi <gabon95@o2.pl>
2018-12-02 12:34:37 +0100

    Added JPA repository implementation for Pets. Closes #39

---
+++ clinicWebData/src/main/java/gabi/clinic/demo/services/springdatajpa/PetSDJpaRepository.java
@@ -0,0 +1,47 @@
+package gabi.clinic.demo.services.springdatajpa;
+
+import gabi.clinic.demo.model.Pet;
+import gabi.clinic.demo.repositories.PetRepository;
+import gabi.clinic.demo.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springbootdata")
+public class PetSDJpaRepository implements PetService{
+    private final PetRepository petRepository;
+
+    public PetSDJpaRepository(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        HashSet<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
